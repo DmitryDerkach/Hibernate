2b9426408d84c16de81add442dc69973c67f7db2
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2022-04-08 12:36:26 +0300

    Info:
    Lesson 15 No code changes
    Lesson 16 logging libraries added (onle pom.xml changed)
    Lesson 17 log4j.xml file added, logger added, code fixes

--- hibernate/src/main/java/com/dmitry/runner/HibernateRunner.java
+++ hibernate/src/main/java/com/dmitry/runner/HibernateRunner.java
@@ -1,52 +1,41 @@
 package com.dmitry.runner;
 
-import java.time.LocalDate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
-import org.hibernate.cfg.Configuration;
-
-import com.dmitry.converter.BirthdayConverter;
-import com.dmitry.entity.Birthday;
-import com.dmitry.entity.Role;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.dmitry.entity.User;
 import com.dmitry.util.HibernateUtil;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 
 public class HibernateRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
+	
 	public static void main(String[] args) {
 		//По отношению к любой из сессий User нах-ся в состоянии Transient
 		User user = User.builder()
-			.username("ivan@gamil.com")
+			.username("ivan@gmail.com")
 			.lastname("Ivanov")
 			.firstname("Ivan")
 			.build();
+		log.info("User entity in transient state, object: {}", user);
 		
-//		try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-//			 Session session1 = sessionFactory.openSession()) {
-//			session1.beginTransaction();
-//			//В 30 строчке User будет в Persistent состоянии по отношению к session1, но все еще в состаянии transient по отношению
-//			//к session 2, потому что не был никак проассоциирован с этой сессией
-//			session1.saveOrUpdate(user);
-//			
-//			//Зыкрываем сессию - сессия перестает существовать
-//			session1.getTransaction().commit();
-//		}
-		
-		try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-				 Session session2 = sessionFactory.openSession()) {
-				//Смотря в дебаге на session2 мы видим, что РС = null
-				session2.beginTransaction();
-				user.setFirstname("Sveta");
-				//В кэшэ появляется user 
-				//session2.delete(user);
-				//session2.refresh(user);
-				//session2.merge(user);
-				session2.refresh(user);
+		try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+			Session session1 = sessionFactory.openSession();
+			try(session1) {
+				var transaction = session1.beginTransaction();
+				log.info("Transaction is created, {}", transaction);
 				
-				//Транзакция завершается user переходит в состояние removed по отношению к session2
-				session2.getTransaction().commit();
+				session1.saveOrUpdate(user);
+				log.info("User is in persistent state: {}, session {}", user, session1);
+			
+				session1.getTransaction().commit();
+			}
+		
+			log.warn("User is in detached state: {}, session is closed {}", user, session1 );
+		} catch (Exception e) {
+			log.error("Exception occurred", e);
+			throw e;
 		}
 		
 		
